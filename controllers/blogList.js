36351bf0f34d5993ef50223338b32d47196525fb
Eloi0424 <beichu0424@qq.com>
2023-11-03 14:54:43 +0800

    homework 4.19

--- controllers/blogList.js
+++ controllers/blogList.js
@@ -1,15 +1,19 @@
 const blogListRouter = require('express').Router()
 const Blog = require('../models/blogList')
 const User = require('../models/user')
-
-
-
+const getTokenFrom = require('../utils/getToken')
+const jwt = require('jsonwebtoken')
 blogListRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
 blogListRouter.post('/', async (request, response) => {
+	const decodedToken = jwt.verify(getTokenFrom(request),process.env.SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' })
+	}
+	const user = await User.findById(decodedToken.id)
 	if(!request.body.title||!request.body.url) {
 		return response.status(400).end()
 	}
@@ -18,11 +22,9 @@ blogListRouter.post('/', async (request, response) => {
 		author: request.body.author,
 		url: request.body.url,
 		likes: request.body.likes||0,
-		user: request.body.user
+		user: user.id
 	})
 	const saveResult = await blog.save()
-	
-	const user = await User.findById(request.body.user)
 	user.blogs = user.blogs.concat(saveResult.id)
 	await user.save()
 	response.status(201).json(saveResult)
