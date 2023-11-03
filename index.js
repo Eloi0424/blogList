4f1b2725b2117183dda6ac1f71afb63dae279c08
Eloi0424 <beichu0424@qq.com>
2023-11-03 14:24:47 +0800

    homework 4.18

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const blogListRouter = require("./controllers/blogList");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
 const errorHandler = require("./utils/errorHandler");
@@ -9,17 +10,12 @@ class App{
 	static instance
 	constructor() {
 		if(!App.instance){
-			const express = require('express')
 			const app = express()
-			const cors = require('cors')
-			const mongoose = require('mongoose')
-			const blogListRouter = require('./controllers/blogList')
-			const config = require('./utils/config')
-			const usersRouter = require("./controllers/users");
 			app.use(cors())
 			app.use(express.json())
 			app.use('/api/blogs', blogListRouter)
 			app.use('/api/users', usersRouter)
+			app.use('/api/login', loginRouter)
 			
 			
 			const PORT = config.PORT
