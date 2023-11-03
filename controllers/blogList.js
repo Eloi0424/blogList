dc0110bd47532bd25d175a164a7b38868c79658c
Eloi0424 <beichu0424@qq.com>
2023-11-03 13:53:45 +0800

    homework 4.17

--- controllers/blogList.js
+++ controllers/blogList.js
@@ -1,11 +1,11 @@
 const blogListRouter = require('express').Router()
 const Blog = require('../models/blogList')
-
+const User = require('../models/user')
 
 
 
 blogListRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
@@ -17,9 +17,14 @@ blogListRouter.post('/', async (request, response) => {
 		title: request.body.title,
 		author: request.body.author,
 		url: request.body.url,
-		likes: request.body.likes||0
+		likes: request.body.likes||0,
+		user: request.body.user
 	})
 	const saveResult = await blog.save()
+	
+	const user = await User.findById(request.body.user)
+	user.blogs = user.blogs.concat(saveResult.id)
+	await user.save()
 	response.status(201).json(saveResult)
 })
 
